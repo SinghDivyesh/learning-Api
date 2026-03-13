f0e83af559f310bb6d6cb807719174a458721cab
Divyesh <singhdivyesh85@gmail.com>
2026-03-13 18:27:54 +0530

    Learning rest api beginner

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+//import 'package:hive_flutter/hive_flutter.dart';
+import 'package:rest_api/screen/home_screen.dart';
+//import 'screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+
+  // Required when using async in main
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive
+  await Hive.initFlutter();
+
+  // Open the favorites box (storage container)
+  await Hive.openBox('favorites');
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'User App',
       debugShowCheckedModeBanner: false,
-      home: UserListScreen(),
-    );
-  }
-}
-
-class UserListScreen extends StatefulWidget {
-  const UserListScreen({super.key});
-
-  @override
-  State<UserListScreen> createState() => _UserListScreenState();
-}
-
-class _UserListScreenState extends State<UserListScreen> {
-  List users = [];
-  bool isLoading = true;
-  @override
-  void initState() {
-    super.initState();
-
-    fetchUsers();
-  }
-
-  Future<void> fetchUsers() async {
-    final response = await http.get(
-      Uri.parse("https://jsonplaceholder.typicode.com/users"),
-    );
-    if (response.statusCode == 200) {
-      setState(() {
-        users = jsonDecode(response.body);
-        isLoading = false;
-      });
-    } else {
-      setState(() {
-        isLoading = false;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            "User from API",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator()):ListView.builder(
-        itemCount: users.length,
-          itemBuilder: (context,index){
-          return Card(
-            margin: EdgeInsets.symmetric(horizontal: 12,vertical: 6),
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.blue,
-                child: Text(users[index]['name'][0],style: TextStyle(color: Colors.white),),
-              ),
-              title: Text(
-                users[index]['name'],
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(users[index]['email']),
-
-            ),
-          );
-          })
-      ,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),
     );
   }
 }
